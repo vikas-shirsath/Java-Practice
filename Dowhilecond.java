39d8c4d33fd790eb500516d0fd2a0ca9a66b8e0f
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2024-05-18 20:38:41 +0530

    Add files via upload

---
+++ Dowhilecond.java
@@ -0,0 +1,13 @@
+public class Dowhilecond {
+    public static void main(String [] args){
+        int i=12;
+
+        while(i<11){
+            System.out.println("Hello World");
+        }
+
+        do{
+            System.out.println("Hello World");
+        }while(i<11);
+    }
+}
