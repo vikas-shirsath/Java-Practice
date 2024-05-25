a6923de8244ce48e141d5eca4e697c2b21238722
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2024-05-25 09:05:49 +0530

    Add files via upload

---
+++ MarksArray.java
@@ -0,0 +1,19 @@
+public class MarksArray {
+    public static void main(String[] args) {
+        int marks[] = new int[3];
+        //int[] marks = new int[3];
+        marks[0] = 97; //Phy
+        marks[1] = 96; //math
+        marks[2] = 98; //Chem
+
+        //System.out.println(marks[0]);
+
+        // int marks[] = {97, 96, 98}
+
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+    
+}
