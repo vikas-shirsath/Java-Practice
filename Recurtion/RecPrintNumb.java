fcc7c575edb8966b2693e25da068c1f8fb3854b4
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2024-06-01 06:53:56 +0530

    Add files via upload

--- Recurtion/RecPrintNumb.java
+++ Recurtion/RecPrintNumb.java
@@ -1,15 +1,15 @@
-//Print numbers in reverse order from 5 with the help of recursion
+//Print numbers rom 1 to 5 with the help of recursion
 
-public class RecPrintRevNumb {
+public class RecPrintNumb {
     public static void printNumb(int n) {
-        if (n ==0 ) {
+        if (n == 6) {
             return;
         }
         System.out.println(n);
-        printNumb(n-1);
+        printNumb(n+1);
     }
     public static void main(String[] args) {
-        int n = 5;
+        int n = 1;
         printNumb(n);
     }
 }
