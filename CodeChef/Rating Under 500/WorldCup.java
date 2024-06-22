7fd1869aff0ef28d4363e60c97c7b6e35c5fd0ed
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2024-06-22 12:53:41 +0530

    Create WorldCup.java

---
+++ CodeChef/Rating Under 500/WorldCup.java
@@ -0,0 +1,20 @@
+//Cricket World Cup Qualifier
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class WorldCup
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		if (x >= 12 ) {
+		    System.out.println("Yes");
+		} else {
+		    System.out.println("No");
+		}
+
+	}
+}
