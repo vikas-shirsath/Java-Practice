87c9c4d23bf1ba83a1f4b5cb4faae94de9ff1933
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2024-06-22 12:54:46 +0530

    Create LuckySeven.java

---
+++ CodeChef/Rating Under 500/LuckySeven.java
@@ -0,0 +1,15 @@
+//Lucky Seven
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in); 
+		String S = sc.nextLine();
+		char luckyChar = S.charAt(6);
+		System.out.println(luckyChar);
+	}
+}
