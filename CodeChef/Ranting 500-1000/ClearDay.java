077d949c75febd192214e2e6c65979f903882ce5
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2024-06-23 15:47:22 +0530

    Add files via upload

---
+++ CodeChef/Ranting 500-1000/ClearDay.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+
+class ClearDay
+{
+	public static void main (String[] args) throws java.lang.Exception	{
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		int Y = sc.nextInt();
+		System.out.println(7 - (X + Y));
+
+	}
+}
+
+
