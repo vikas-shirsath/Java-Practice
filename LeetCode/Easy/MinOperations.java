2bfb131b3f87d46a6b41c7b40177aa985673c5c3
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2024-08-08 00:02:53 +0530

    leetCode

---
+++ LeetCode/Easy/MinOperations.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int count = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i]%3==0) {
+                continue;
+            } else{
+                count++;
+            }
+        }
+        return count;
+    }
+}
