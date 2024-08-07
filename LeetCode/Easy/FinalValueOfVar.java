4f6bed665eadfe4337d5de6703582a755ba7da59
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2024-08-08 00:21:51 +0530

    leetCode

---
+++ LeetCode/Easy/FinalValueOfVar.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x = 0;
+        for(int i=0; i<operations.length; i++) {
+            switch (operations[i]) {
+                case "--X" :
+                case "X--" :
+                    x--;
+                    break;
+                case "++X" :
+                case "X++" :
+                    x++;
+                    break;
+            }
+        }
+        return x;
+    }
+}
