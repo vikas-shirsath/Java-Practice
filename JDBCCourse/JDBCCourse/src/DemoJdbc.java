66e3079890bbef9f5b1b83109bece79e9459db49
Vikas Pandharinath Shirsath <156660115+vikas47s@users.noreply.github.com>
2024-10-18 00:07:29 +0530

    Practice

--- JDBCCourse/JDBCCourse/src/DemoJdbc.java
+++ JDBCCourse/JDBCCourse/src/DemoJdbc.java
@@ -1,7 +1,4 @@
-import java.sql.Array;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DemoJdbc {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
@@ -16,11 +13,14 @@ public class DemoJdbc {
         String url = "jdbc:postgresql://localhost:5432/Demo";
         String uname = "postgres";
         String pass = "2448";
+        String sql = "select sname from student where sid = 1";
 
         Class.forName("org.postgresql.Driver");  //not compulsory
 
         Connection con = DriverManager.getConnection(url,uname,pass);
 
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(sql); //for fetching data
         System.out.println("Connection established");
 
 
