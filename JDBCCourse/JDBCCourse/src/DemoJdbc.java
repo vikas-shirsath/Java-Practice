cb626ef4dcd2e8f8968b9d04907f684e595fd6dd
Vikas Pandharinath Shirsath <156660115+vikas47s@users.noreply.github.com>
2024-10-17 23:47:32 +0530

    Practice

--- JDBCCourse/JDBCCourse/src/DemoJdbc.java
+++ JDBCCourse/JDBCCourse/src/DemoJdbc.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.sql.Array;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DemoJdbc {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // import the package
+        // load and register
+        // create connection
+        // create statement
+        // execute the statement
+        // process the results
+        // close
+
+        Class.forName("org.postgresql.Driver");
+
+        Connection con = DriverManager.getConnection("","","");
+
+
+    }
 }
